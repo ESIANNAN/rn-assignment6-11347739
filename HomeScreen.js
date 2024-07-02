b6f818b2944afcd510e5d7903eed08da71a82c7d
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-07-02 03:30:15 +0000

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -3,24 +3,27 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
+<View style={styles.container}>
     <View style={styles.navigationBar}>
-      <Image source={require('./assets/Menu.png')} style={styles.navigationIcon} />
-      <Image source={require('./assets/Logo.png')} style={styles.navigationIcon} />
-      <Image source={require('./assets/Search (1).png')} style={styles.naviggationIcon} />
-      <Image source={require('./assets/shoppingBag.png')} style={styles.navigationIcon} />
-      <StatusBar style="auto" />
-    </View>
+      <Image source={require('./assets/Menu.png')} style={styles.menuIcon} />
+      <Image source={require('./assets/Logo.png')} style={styles.logoIcon} />
+      <Image source={require('./assets/Search (1).png')} style={styles.searchIcon} />
+      <Image source={require('./assets/shoppingBag.png')} style={styles.shoppingIcon} />
+</View>
+
+
+
+    <StatusBar style="auto" />
+ </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
- navigationBar: {
+  navigationBar: {
     flexDirection: 'row',
     flex: 1,
     marginTop: 70,
