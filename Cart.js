562a1efba90a54f260998c440ee14182190db03c
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-07-03 02:46:39 +0000

    Update Cart.js
    modified the appearance of the screen and the icons

--- Cart.js
+++ Cart.js
@@ -21,7 +21,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  navigationBar: {
+    flexDirection: 'row',
+    marginTop: 60,
+  }
 });
