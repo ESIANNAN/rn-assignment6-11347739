f6ef9ef26e2468e280c1ef8cd221346f61bd1f5d
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-07-03 22:33:25 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
@@ -7,12 +7,18 @@ import Cart from './Cart';
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [cart, setCart] = React.useState([]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Cart" component={Cart} />
+        <Stack.Screen name="Home">
+          {props => <HomeScreen {...props} cart={cart} setCart={setCart} />}
+        </Stack.Screen>
+        <Stack.Screen name="Cart">
+          {props => <Cart {...props} cart={cart} setCart={setCart} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  );
 }
